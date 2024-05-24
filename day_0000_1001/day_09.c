20b03495f9bda0b9f738b9b61f1a9b71edd6ba72
Jordan <italia.france30@gmail.com>
2024-05-23 17:32:23 -0700

    day 9

--- day_0000_1001/day_09.c
+++ day_0000_1001/day_09.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Node {
+    long long data;
+    struct Node* next;
+};
+typedef struct Node Queue;
+
+void part1(int size, int preamble, long long nums[]);
+void enqueue(Queue** head, Queue** tail, long long data);
+long long dequeue(Queue** head, Queue** tail);
+void releaseNodes(Queue** head, Queue** tail);
+
 long int getFileSize(char file_name[]) {
     FILE* file = fopen(file_name, "r");
 
@@ -12,11 +23,6 @@ long int getFileSize(char file_name[]) {
     fclose(file);
     return sz;
 }
-
-struct Node {
-    int x;
-    struct Node* next;
-};
 
 int main() {
     char file_name[] = "day_09.txt";
@@ -52,12 +58,12 @@ int main() {
     }
 
     char n[digit_length];
-    int nums[new_lines];
+    long long nums[new_lines];
     int i, j, k;
     for(i=0, j=0, k=0; str[i] != '\0'; i++, j++) {
         if( str[i] == '\n' ) {
             n[j] = '\0';
-            nums[k] = atoi(n);
+            nums[k] = atoll(n);
             k++;
             j = -1;
             continue;
@@ -65,11 +71,109 @@ int main() {
         n[j] = str[i];
     }
     n[j] = '\0';
-    nums[k] = atoi(n);
+    nums[k] = atoll(n);
 
-
+    part1(new_lines, preamble_length, nums);
 }
 
-void part1() {
+void part1(int size, int preamble, long long nums[]) {
+    Queue* head;
+    Queue* tail;
+
+    head = tail = NULL;
+
+    int i;
+    long long sum = 0, target;
+
+    for(i=0; i<preamble; i++) {
+        enqueue(&head, &tail, nums[i]);
+    }
+
+    for(i; i<size; i++) {
+        Queue* cursor = head;
+        while(sum != nums[i] && cursor != NULL) {
+            Queue* cursori = cursor->next;
+            while(sum != nums[i] && cursori != NULL) {
+                sum = cursor->data + cursori->data;
+                cursori = cursori->next;
+            }
+            if( sum != nums[i] )
+                cursor = cursor->next;
+        }
+        if( cursor == NULL ) {
+            printf("Could not find two preamble numbers that sum to %llu\n", nums[i]);
+            target = nums[i];
+            break;
+        }
+
+        dequeue(&head, &tail);
+        enqueue(&head, &tail, nums[i]);
+    }
+
+    releaseNodes(&head, &tail);
+
+    for(i=0; i<size; i++) {
+        sum = nums[i]; 
+        for(int j=i+1; sum<target && j<size; j++) {
+            sum += nums[j];
+            if( sum == target ) {
+                long long min = 922340000000000000;
+                long long max = 0;
+                for(int k=i; k<=j; k++) {
+                    if( min > nums[k] )
+                        min = nums[k];
+                    if( max < nums[k] ) 
+                        max = nums[k];
+                }
+                printf("Min + Max = %llu\n", (min + max));
+            }
+        }
+        if( sum == target) 
+            break;
+    }
+}
+
+void enqueue(Queue** head, Queue** tail, long long data) {
+    Queue* cursor = *tail;
+
+    Queue* newNode = (Queue*) malloc(sizeof(Queue));    
+
+    newNode->data = data;
+    newNode->next = NULL;
     
+    if( *head == NULL ) {
+        *head = *tail = newNode;
+        return;
+    }
+
+    cursor->next = newNode;
+    *tail = newNode;
+}
+
+long long dequeue(Queue** head, Queue** tail) {
+    Queue* oblivion = *head;
+
+    int data = oblivion->data;
+
+    if( head == tail ) 
+        *head = *tail = NULL;
+    else 
+        *head = oblivion->next;
+
+    free(oblivion);
+
+    return data;
+}
+
+void releaseNodes(Queue** head, Queue** tail) {
+    Queue* cursor = *head;
+    Queue* next;
+
+    while( cursor != NULL ) {
+        next = cursor->next;
+        free(cursor);
+        cursor = next;
+    }
+
+    head = tail = NULL;
 }
